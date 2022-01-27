628f1e36c0fec1611595613fca336bea9fb3b34b
ferrol12108428 <ferroldimasstyadijaya@smkwikrama>
2022-01-27 07:58:03 +0700

    update index.php

--- index.php
+++ index.php
@@ -3,7 +3,6 @@
 	    <title>PHP Test</title>
 	  </head>
 	  <body>   
-	 <!-- penanganan form dengan method POST -->
 	<form method="post" action="tampil.php">
 		<label>Masukkan Nama</label><br/>
 		<input type="text" name="nama"><br/>
